07e985fe404ca2663f1e9579f5505c89dde87b4a
KaileyHart <hartkailey0@gmail.com>
2024-03-28 15:12:47 -0400

    Add new component and utilities

---
+++ components/ValueCard.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { isEmpty } from "../utilities/sharedFunctions";
+
+const ValueCard = (props) => {
+
+  let label = isEmpty(props) === false && isEmpty(props.label) === false ? props.label : null;
+  let value = isEmpty(props) === false && isEmpty(props.value) === false ? props.value : null;
+
+  return (
+    <View style={styles.valueContainer}> 
+      <Text style={styles.label}>{label}</Text>
+      <Text style={styles.value}>{value}</Text>
+    </View>
+  );
+
+}
+
+export default ValueCard;
+
+const styles = StyleSheet.create({
+  valueContainer: {
+
+  },
+  label: {
+    fontSize: 20,
+    color: "#fff"
+  },
+  value: {
+    fontSize: 35,
+    color: "#AFB3BE",
+    fontWeight: "500"
+  }
+});
